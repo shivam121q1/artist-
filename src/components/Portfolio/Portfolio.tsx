d7ba37cf98e67616a4f6ea2e3b0ea8a7674c42ca
shivam121q1 <shivampal7006@gmail.com>
2025-02-01 16:13:37 +0530

    Changes

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -3,25 +3,20 @@ import Image from "next/image";
 
 export default function PortfolioHighlights() {
   return (
-    <div className="relative flex flex-col items-center justify-center py-10 px-4 bg-primary opacity-8080">
-      <h2 className="text-white text-3xl font-bold mb-6">
+    <div className="relative flex flex-col items-center justify-center py-10 px-4 bg-primary">
+      <h2 className="text-white text-3xl font-bold mb-6 text-center">
         <span className="text-white font-semibold">Portfolio</span> Highlights
       </h2>
-      <div className="grid grid-cols-3 gap-6">
-
-        {imgPortfolioData.map((item,index)=>(
-            <div key={index} className="rounded-lg overflow-hidden w-72 h-72">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 w-full max-w-6xl">
+        {imgPortfolioData.map((item, index) => (
+          <div key={index} className="rounded-lg overflow-hidden w-full h-60 sm:h-72">
             <img
               src={item.img}
               alt="Book and Camera"
-              className="rounded-lg w-72 h-72"
+              className="rounded-lg w-full h-full object-cover"
             />
           </div>
-
         ))}
-        {/* Image 1 */}
-        
-        
       </div>
     </div>
   );
